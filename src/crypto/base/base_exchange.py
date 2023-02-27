a90aea29338fd6eec9e2db7bbf4e8845f192fd25
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-02-27 17:11:05 +0200

    feat(crypto.base): add internal_id for base classes

--- src/crypto/base/base_exchange.py
+++ src/crypto/base/base_exchange.py
@@ -7,11 +7,13 @@ from .base_ticker_client import BaseTickerClient
 
 
 class BaseExchange(ABC):
+    internal_id: int
     ticker_client: BaseTickerClient
 
-    def __init__(self) -> None:
+    def __init__(self, internal_id) -> None:
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.ticker_client = self.get_ticker_client()(self.name)
+        self.internal_id = internal_id
+        self.ticker_client = self.get_ticker_client()(self.name, internal_id)
 
     @property
     @abstractmethod
