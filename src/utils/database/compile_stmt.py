5d68da61f134a6773f1088ee848dc4fad22e8538
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-02-28 18:37:17 +0200

    feat(utils): edit compile_stmt function

--- src/utils/database/compile_stmt.py
+++ src/utils/database/compile_stmt.py
@@ -1,11 +1,9 @@
+from typing import Tuple
+
 from sqlalchemy.dialects import mysql
 from sqlalchemy.dialects.mysql.dml import ClauseElement
 
 
-def compile_stmt(stmt: ClauseElement) -> str:
-    return str(
-        stmt.compile(
-            compile_kwargs={"literal_binds": True},
-            dialect=mysql.dialect()
-        )
-    )
+def compile_stmt(stmt: ClauseElement) -> Tuple[str, tuple]:
+    stmt_compiled = stmt.compile(dialect=mysql.dialect())
+    return (str(stmt_compiled), tuple(stmt_compiled.params.values()))
