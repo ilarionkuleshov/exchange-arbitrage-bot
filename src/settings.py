a38758fed05bc54d492e55eb4b16f43fd0ebf4c6
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-02-28 22:15:12 +0200

    feat(settings): add user agent

--- src/settings.py
+++ src/settings.py
@@ -24,6 +24,8 @@ REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+
 ########### Other Settings ###########
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
