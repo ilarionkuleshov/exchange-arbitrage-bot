84a0eafbf1827ff059b1c1701e0db8d7d9c404ff
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-03-01 00:57:26 +0200

    feat(interfaces): add from_db classmethod for session_settings

--- src/interfaces/session_settings.py
+++ src/interfaces/session_settings.py
@@ -1,5 +1,13 @@
+import json
+
 from typing import Dict
 from dataclasses import dataclass, field
+
+from sqlalchemy import create_engine, select
+
+from database.models import Session
+from utils import safe_execute
+from utils.database import mysql_connection_url
 
 
 @dataclass
@@ -9,3 +17,24 @@ class SessionSettings:
     quote_currency: str
     min_quote_volume_24h: int
     exchanges: Dict[str, int] = field(default_factory=dict)
+
+    @classmethod
+    def from_db(cls, raw_session_id: str):
+        session_id = safe_execute(int, raw_session_id)
+        if session_id is None:
+            raise Exception("Invalid session_id was passed")
+        with create_engine(mysql_connection_url()).connect() as connection:
+            raw_session_settings = connection.execute(
+                select(Session.settings).where(Session.id == session_id)
+            )
+            raw_session_settings = list(raw_session_settings)
+            if len(raw_session_settings) != 1 or len(raw_session_settings[0]) != 1:
+                raise Exception(
+                    f"Session with id = {session_id} does not exist in the database"
+                )
+            session_settings = safe_execute(json.loads, raw_session_settings[0][0])
+            if session_settings is None:
+                raise Exception(
+                    f"Invalid settings specified for the session (id = {session_id})"
+                )
+            return cls(session_id, **session_settings)
