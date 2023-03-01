f2793bef9b2086dbf30313aba45d5d064419c9cd
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-03-01 22:25:09 +0200

    fix(pm2): add python_interpreter to pm2 config

--- pm2/pm2.config.js
+++ pm2/pm2.config.js
@@ -1,13 +1,20 @@
 const path = require("path");
 
-if (process.platform === "win32") scrapy_script = path.join("..", "src", ".venv", "Scripts", "scrapy.exe");
-else scrapy_script = path.join("..", "src", ".venv", "bin", "scrapy");
+if (process.platform === "win32") {
+    scrapy_script = path.join("..", "src", ".venv", "Scripts", "scrapy.exe");
+    python_interpreter = path.join("..", "src", ".venv", "Scripts", "python.exe");
+}
+else {
+    scrapy_script = path.join("..", "src", ".venv", "bin", "scrapy");
+    python_interpreter = path.join("..", "src", ".venv", "bin", "python");
+}
 
 module.exports = {
     apps: [
         {
             name: "eab_session_runner",
             script: scrapy_script,
+            interpreter: python_interpreter,
             args: "session_runner",
             cwd: path.join("..", "src"),
             log_file: path.join("..", "logs", "eab_session_runner.log"),
