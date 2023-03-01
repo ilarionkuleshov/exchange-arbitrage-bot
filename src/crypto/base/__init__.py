5819a0df71e59476eccf99219f9c5c2ad201888a
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-03-01 15:02:32 +0200

    feat(crypto.base): add base_url_builder_client

--- src/crypto/base/__init__.py
+++ src/crypto/base/__init__.py
@@ -1,3 +1,4 @@
 from .base_client import BaseClient
 from .base_ticker_client import BaseTickerClient
 from .base_manager import BaseManager
+from .base_url_builder_client import BaseUrlBuilderClient
