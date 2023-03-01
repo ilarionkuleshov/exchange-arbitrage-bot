19fcdc1037c845f5889f985216fa53b341eb6d42
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-03-01 16:33:18 +0200

    feat(utils): update task_status_codes

--- src/utils/task_status_codes.py
+++ src/utils/task_status_codes.py
@@ -4,7 +4,9 @@ from enum import IntEnum
 class TaskStatusCodes(IntEnum):
     NOT_PROCESSED = 0
     IN_PROCESSING = 1
+
     SUCCESS = 2
+    MESSAGE_SENT_SUCCESS = 22
 
     ERROR = 4
     BANNED = 44
