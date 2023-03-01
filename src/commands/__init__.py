89652c6c35848a86d1504fff766ff1b46f62ba27
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-03-01 21:01:29 +0200

    feat(commands): add session_runner

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -2,3 +2,4 @@ from .exchange_initializer import ExchangeInitializer
 from .dual_comparer import DualComparer
 from .db_cleaner import DBCleaner
 from .tg_sender import TGSender
+from .session_runner import SessionRunner
