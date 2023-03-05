2e1befbd75d4bc5228f1ef3de7a78758654c3f56
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-03-05 22:27:04 +0200

    feat(pipelines): edit last_trade_db_pipeline

--- src/pipelines/last_trade_db_pipeline.py
+++ src/pipelines/last_trade_db_pipeline.py
@@ -4,16 +4,19 @@ from sqlalchemy import update
 from MySQLdb.cursors import DictCursor
 
 from .base import BaseDBPipeline
-from items import MarketItem
+from items import LastTradeItem
 from database.models import Market
 from utils.database import compile_stmt
 
 
 class LastTradeDBPipeline(BaseDBPipeline):
     def process_item(self, item: Item, spider: Spider) -> Item:
-        if isinstance(item, MarketItem):
+        if isinstance(item, LastTradeItem):
             stmt = update(Market).values(
-                price=item["price"]
+                price=item["price"],
+                status=item["status"]
+            ).where(
+                Market.id == item["market_id"]
             )
             d = self.db_connection_pool.runInteraction(self.update_market, stmt)
             d.addCallback(self.item_stored)
