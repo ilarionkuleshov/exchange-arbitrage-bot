5dc24a018053bf23fdd1049d9012c2978a13782f
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-03-05 19:09:53 +0200

    feat(pipelines): add base_db_pipeline

--- src/pipelines/ticker_db_pipeline.py
+++ src/pipelines/ticker_db_pipeline.py
@@ -1,43 +1,15 @@
-import logging
-
-from scrapy import Spider, Item, signals
-from scrapy.crawler import Crawler
-from scrapy.exceptions import DontCloseSpider
-
-from twisted.python.failure import Failure
-from twisted.enterprise.adbapi import ConnectionPool
+from scrapy import Spider, Item
 
 from MySQLdb.cursors import DictCursor
 from sqlalchemy.dialects.mysql import insert
 
+from .base import BaseDBPipeline
 from items import MarketItem
 from database.models import Market
-from utils.database import open_db_connection_pool, compile_stmt
+from utils.database import compile_stmt
 
 
-class TickerDBPipeline:
-    db_connection_pool: ConnectionPool
-
-    def __init__(self) -> None:
-        self.logger = logging.getLogger(self.__class__.__name__)
-        self.pending_items = 0
-
-    @classmethod
-    def from_crawler(cls, crawler: Crawler) -> "TickerDBPipeline":
-        o = cls()
-        crawler.signals.connect(o.spider_idle, signal=signals.spider_idle)
-        return o
-
-    def open_spider(self, spider: Spider) -> None:
-        self.db_connection_pool = open_db_connection_pool()
-
-    def close_spider(self, spider: Spider) -> None:
-        self.db_connection_pool.close()
-
-    def spider_idle(self, spider: Spider) -> None:
-        if self.pending_items > 0:
-            raise DontCloseSpider
-
+class TickerDBPipeline(BaseDBPipeline):
     def process_item(self, item: Item, spider: Spider) -> Item:
         if isinstance(item, MarketItem):
             stmt = insert(Market).values(item).on_duplicate_key_update(
@@ -54,9 +26,3 @@ class TickerDBPipeline:
 
     def insert_market(self, transaction: DictCursor, stmt) -> None:
         transaction.execute(*compile_stmt(stmt))
-
-    def item_stored(self, _) -> None:
-        self.pending_items -= 1
-
-    def errback(self, failure: Failure) -> None:
-        self.logger.error(failure.getErrorMessage())
