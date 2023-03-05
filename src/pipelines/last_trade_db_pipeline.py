2647a4f68ec259b14791db789b6f0294184ccdd0
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2023-03-05 19:14:03 +0200

    feat(pipelines): add last_trade_db_pipeline

---
+++ src/pipelines/last_trade_db_pipeline.py
@@ -0,0 +1,27 @@
+from scrapy import Spider, Item
+
+from sqlalchemy import update
+from MySQLdb.cursors import DictCursor
+
+from .base import BaseDBPipeline
+from items import MarketItem
+from database.models import Market
+from utils.database import compile_stmt
+
+
+class LastTradeDBPipeline(BaseDBPipeline):
+    def process_item(self, item: Item, spider: Spider) -> Item:
+        if isinstance(item, MarketItem):
+            stmt = update(Market).values(
+                price=item["price"]
+            )
+            d = self.db_connection_pool.runInteraction(self.update_market, stmt)
+            d.addCallback(self.item_stored)
+            d.addErrback(self.errback)
+            self.pending_items += 1
+        else:
+            self.logger.warning(f"Undefined item instance ({type(item)})")
+        return item
+
+    def update_market(self, transaction: DictCursor, stmt) -> None:
+        transaction.execute(*compile_stmt(stmt))
